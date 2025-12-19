8cfc0d7a97dd1f2a781af5bdb1553b2a77ab05a1
alexzaborskikh <alexzaborskikh@gmail.com>
2025-12-19 18:34:42 +0000

    Create Clinician.java

---
+++ src/uk/ac/healthcare/New folder/Clinician.java
@@ -0,0 +1,22 @@
+package uk.ac.healthcare.model;
+
+public abstract class Clinician extends User {
+    protected final String clinicianId;
+    protected final String speciality;
+    protected final String registrationNo; // e.g., GMC/NMC
+    protected final String workplaceId;
+
+    protected Clinician(String clinicianId, String firstName, String lastName, String email,
+                        String speciality, String registrationNo, String workplaceId) {
+        super(clinicianId, firstName, lastName, email);
+        this.clinicianId = clinicianId;
+        this.speciality = speciality;
+        this.registrationNo = registrationNo;
+        this.workplaceId = workplaceId;
+    }
+
+    public String getClinicianId() { return clinicianId; }
+    public String getSpeciality() { return speciality; }
+    public String getRegistrationNo() { return registrationNo; }
+    public String getWorkplaceId() { return workplaceId; }
+}
