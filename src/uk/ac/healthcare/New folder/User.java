a98fda13aa52b5ad28cba7f29d82800367ba163c
alexzaborskikh <alexzaborskikh@gmail.com>
2025-12-19 18:19:09 +0000

    Create User.java
    abstract class, will be extended in future by other classes

---
+++ src/uk/ac/healthcare/New folder/User.java
@@ -0,0 +1,22 @@
+package uk.ac.healthcare.model;
+
+public abstract class User {
+    protected final String userId;
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+
+    protected User(String userId, String firstName, String lastName, String email) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getUserId() { return userId; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getEmail() { return email; }
+
+    public String getDisplayName() { return firstName + " " + lastName; }
+}
