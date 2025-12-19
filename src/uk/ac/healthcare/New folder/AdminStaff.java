b6e3cdc8153f82d9a0fd541df6c648ecd2a90176
alexzaborskikh <alexzaborskikh@gmail.com>
2025-12-19 18:24:55 +0000

    Created PAtient and AdminStaff class

---
+++ src/uk/ac/healthcare/New folder/AdminStaff.java
@@ -0,0 +1,19 @@
+package uk.ac.healthcare.model;
+
+public class AdminStaff extends User {
+    private final String role;
+    private final String facilityId;
+    private final String accessLevel;
+
+    public AdminStaff(String staffId, String firstName, String lastName, String email,
+                      String role, String facilityId, String accessLevel) {
+        super(staffId, firstName, lastName, email);
+        this.role = role;
+        this.facilityId = facilityId;
+        this.accessLevel = accessLevel;
+    }
+
+    public String getRole() { return role; }
+    public String getFacilityId() { return facilityId; }
+    public String getAccessLevel() { return accessLevel; }
+}
---
+++ src/uk/ac/healthcare/New folder/Patient.java
@@ -0,0 +1,15 @@
+package uk.ac.healthcare.model;
+
+public class Patient extends User {
+    private final String nhsNumber;
+    private final String registeredSurgeryId;
+
+    public Patient(String patientId, String firstName, String lastName, String email, String nhsNumber, String registeredSurgeryId) {
+        super(patientId, firstName, lastName, email);
+        this.nhsNumber = nhsNumber;
+        this.registeredSurgeryId = registeredSurgeryId;
+    }
+
+    public String getNhsNumber() { return nhsNumber; }
+    public String getRegisteredSurgeryId() { return registeredSurgeryId; }
+}
